8988cf53ad95396cef161ce5a4644f2dcb417b4a
Bao <64295746+CinnaBao@users.noreply.github.com>
2020-05-24 15:42:03 +0800

    Update SpaCy_main.py

--- SpaCy_main.py
+++ SpaCy_main.py
@@ -1,8 +1,6 @@
 
 # coding: utf-8
-
-# In[ ]:
-
+# Author: Bao Wan-Yun
 
 import Data_preprocessing as Factory
 import TrainingSpaCy as Factory2
@@ -11,4 +9,5 @@ P = Factory.Data_preprocessing()
 T = Factory2.TrainingSpaCy()
 Training_data = P.Preprocessing('Resumes.json')  
 Recognizer = T.Recognizer(Training_data)
-
+Entities_list = Recognizer[1]
+Recognizer = Recognizer[0]
